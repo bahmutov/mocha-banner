471b117b7c8e87d540ed5f7a526d38039ea89125
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-12 15:04:49 -0500

    init code

---
+++ src/mocha-banner-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const mochaBanner = require('.')
+
+describe('mocha-banner', () => {
+  it('write this test', () => {
+    console.assert(mochaBanner, 'should export something')
+  })
+})
